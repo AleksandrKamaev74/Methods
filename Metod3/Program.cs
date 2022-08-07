b63e755d01f3a2ea3439af5a39c039748a7b26f9
Aleksandr Kamaev <al.kamaev@yandex.ru>
2022-08-07 20:10:31 +0300

    Программа вывода на экран массива из 8 элементов

---
+++ Metod3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите метод(ы), которай задаёт массив из 8 элементов и 
+//выводит их на экран.
+void Massive()
+{
+  int[]array=new int[8];
+  Random rnd=new Random();
+  for (int i=0;i<array.Length;i=i+1)
+  { 
+    array[i]=rnd.Next(1,101);
+    if(i==array.Length-1)
+    {
+     Console.Write(array[i]+" ");   
+    }
+    else 
+     Console.Write(array[i]+","); 
+       
+  }
+}
+Massive();
+
+
+
+
